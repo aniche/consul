6db7ae983de862f49a2992bab68ae03dc84ddd61
uri <uri.lazar@gmail.com>
2018-02-11 20:09:34 +0000

    d

--- app/helpers/custom_urls_helper.rb
+++ app/helpers/custom_urls_helper.rb
@@ -1,8 +1,10 @@
 module CustomUrlsHelper
+  def legislation_question_path(question)
+    legislation_process_question_url(question.process, question)
+  end
   def legislation_question_url(question)
     legislation_process_question_url(question.process, question)
   end
-
   def legislation_annotation_url(annotation)
     legislation_process_question_url(annotation.draft_version.process, annotation.draft_version, annotation)
   end
